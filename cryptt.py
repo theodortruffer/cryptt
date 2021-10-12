f066465219931a5c0e8bd9231c25d37c9fc3481e
Theodor Truffer <theo@fluxlabs.ch>
2021-10-12 17:05:27 +0200

    rc1

--- cryptt.py
+++ cryptt.py
@@ -2,120 +2,62 @@ import os
 from io import BufferedReader
 
 import click
-from Crypto.Cipher import AES
-from Crypto.Util import Padding
-
-
-def decrypt_eax(input_file, output_file, key_file, block_cypher_mode):
-    nonce, tag, ciphertext = [input_file.read(x) for x in (16, 16, -1)]
-    cipher = AES.new(__check_and_read_key(key_file), block_cypher_mode, nonce)
-    data = cipher.decrypt_and_verify(ciphertext, tag)
-    output_file.write(data)
-
-
-def decrypt_ecb(input_file, output_file, key_file, block_cypher_mode):
-    ciphertext = input_file.read()
-    cipher = AES.new(__check_and_read_key(key_file), block_cypher_mode)
-    plaintext = Padding.unpad(cipher.decrypt(ciphertext), BLOCK_SIZE)
-    output_file.write(plaintext)
-
-
-def decrypt_classic(input_file, output_file, key_file, block_cypher_mode):
-    ciphertext = input_file.read()
-    iv = ciphertext[:BLOCK_SIZE]
-    cipher = AES.new(__check_and_read_key(key_file), block_cypher_mode, iv)
-    plaintext = Padding.unpad(cipher.decrypt(ciphertext[BLOCK_SIZE:]), BLOCK_SIZE)
-    output_file.write(plaintext)
-
-
-def encrypt_ecb(input_file, output_file, key_file, block_cypher_mode):
-    cipher = AES.new(__check_and_read_key(key_file), block_cypher_mode)
-    plaintext = Padding.pad(input_file.read(), BLOCK_SIZE)
-    ciphertext = cipher.encrypt(plaintext)
-    output_file.write(ciphertext)
-
-
-def encrypt_classic(input_file, output_file, key_file, block_cypher_mode):
-    iv = os.urandom(BLOCK_SIZE)
-    plaintext = Padding.pad(input_file.read(), BLOCK_SIZE)
-    cipher = AES.new(__check_and_read_key(key_file), block_cypher_mode, iv)
-    ciphertext = cipher.encrypt(plaintext)
-    [output_file.write(x) for x in (iv, ciphertext)]
-
-
-def encrypt_eax(input_file, output_file, key_file, block_cypher_mode):
-    cipher = AES.new(__check_and_read_key(key_file), block_cypher_mode)
-    ciphertext, tag = cipher.encrypt_and_digest(input_file.read())
-    [output_file.write(x) for x in (cipher.nonce, tag, ciphertext)]
-
-
-block_cypher_modes = {
-    'ECB': {
-        'encrypt_fn': encrypt_ecb,
-        'decrypt_fn': decrypt_ecb,
-        'mode': AES.MODE_ECB
-    },
-    'CBC': {
-        'encrypt_fn': encrypt_classic,
-        'decrypt_fn': decrypt_classic,
-        'mode': AES.MODE_CBC
-    },
-    'CTR': {
-        'encrypt_fn': encrypt_classic,
-        'decrypt_fn': decrypt_classic,
-        'mode': AES.MODE_CTR
-    },
-    'CFB': {
-        'encrypt_fn': encrypt_classic,
-        'decrypt_fn': decrypt_classic,
-        'mode': AES.MODE_CFB
-    },
-    'OFB': {
-        'encrypt_fn': encrypt_classic,
-        'decrypt_fn': decrypt_classic,
-        'mode': AES.MODE_OFB
-    },
-    'AEAD': {
-        'encrypt_fn': encrypt_eax,
-        'decrypt_fn': decrypt_eax,
-        'mode': AES.MODE_EAX
-    }
-}
-
-BLOCK_SIZE = 16
+from CrypttService import CrypttService
 
 
 @click.group()
 def cli():
+    """Encryption and decryption of files, using a range of algorithms and block cipher modes"""
     pass
 
 
 @cli.command()
 @click.argument('input_file', type=click.File('rb'))
-@click.argument('output_file', type=click.File('xb'))
+@click.argument('output', type=click.File('xb'))
 @click.argument('key_file', type=click.File('rb'))
-@click.option('--block_cypher_mode',
-              type=click.Choice(list(block_cypher_modes.keys()), case_sensitive=False), prompt=True)
-def encrypt(input_file, output_file, key_file, block_cypher_mode):
-    mode_conf = block_cypher_modes[block_cypher_mode]
-    mode_conf['encrypt_fn'](input_file, output_file, key_file, mode_conf['mode'])
+@click.option('--block_cipher_mode',
+              type=click.Choice(list(CrypttService.get_block_cipher_modes_config().keys()), case_sensitive=False),
+              prompt=True)
+@click.option('--algorithm',
+              type=click.Choice(list(CrypttService.get_algorithms_config().keys()), case_sensitive=False),
+              default='AES')
+def encrypt(input_file: BufferedReader, output: BufferedReader, key_file: BufferedReader, block_cipher_mode: str,
+            algorithm: str):
+    """INPUT_FILE: path of file to be encrypted (must exist, or use "-" for stdin)\n
+        OUTPUT: path of where the encrypted file will be stored (must not exist, use "-" for stdout)\n
+        KEY_FILE: path of file with a base64 encrypted key (required key size differs depending on algorithm)"""
+    try:
+        key = key_file.read().decode()
+        service = CrypttService(block_cipher_mode, algorithm, key)
+        plaintext = input_file.read().decode()
+        cipher_text = service.encrypt(plaintext)
+        output.write(cipher_text)
+    except Exception:
+        os.remove(output.name)
+        raise
 
 
 @cli.command()
 @click.argument('input_file', type=click.File('rb'))
-@click.argument('output_file', type=click.File('xb'))
+@click.argument('output', type=click.File('xb'))
 @click.argument('key_file', type=click.File('rb'))
-@click.option('--block_cypher_mode',
-              type=click.Choice(list(block_cypher_modes.keys()), case_sensitive=False), prompt=True)
-def decrypt(input_file, output_file, key_file, block_cypher_mode):
-    mode_conf = block_cypher_modes[block_cypher_mode]
-    mode_conf['decrypt'](input_file, output_file, key_file, mode_conf['mode'])
-
-
-def __check_and_read_key(key_file: BufferedReader):
-    key_chunk = key_file.read(64)
-    key_size = len(key_chunk)
-    if key_size not in [16, 24, 32]:
-        click.echo("ERROR: key length must be 16, 24 or 32 bytes, but was " + str(key_size) + " bytes")
-        exit(1)
-    return key_chunk
+@click.option('--block_cipher_mode',
+              type=click.Choice(list(CrypttService.get_block_cipher_modes_config().keys()), case_sensitive=False),
+              prompt=True)
+@click.option('--algorithm',
+              type=click.Choice(list(CrypttService.get_algorithms_config().keys()), case_sensitive=False),
+              default='AES')
+def decrypt(input_file: BufferedReader, output: BufferedReader, key_file: BufferedReader, block_cipher_mode: str,
+            algorithm: str):
+    """INPUT_FILE: path of file to be decrypted (must exist, or use "-" for stdin)\n
+        OUTPUT: path of where the decrypted file will be stored (must not exist, use "-" for stdout)\n
+        KEY_FILE: path of file with a base64 encrypted key (required key size differs depending on algorithm)"""
+    try:
+        key = key_file.read().decode()
+        service = CrypttService(block_cipher_mode, algorithm, key)
+        cipher_text = input_file.read().decode()
+        plaintext = service.decrypt(cipher_text)
+        output.write(plaintext)
+    except Exception:
+        os.remove(output.name)
+        raise
